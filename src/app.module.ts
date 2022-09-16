a8b2fe3b57e5a5c84ee5c5219eb1799853783f88
Oscar Javier Jiménez <o.jimenezm@uniandes.edu.co>
2022-09-15 21:26:41 -0500

    Feature/persistence (#2)
    * 📦install typeorm package
    
    * ✈add airline and airport modules and entities
    
    * 📦install pg packages
    
    * ❌drop postgres-data
    
    * 🐋create docker compose file
    
    * 💾add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AirlineModule } from './airline/airline.module';
 import { AirportModule } from './airport/airport.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AirlineEntity } from './airline/airline.entity';
+import { AirportEntity } from './airport/airport.entity';
 
 @Module({
-  imports: [AirlineModule, AirportModule],
+  imports: [AirlineModule, AirportModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'airportDB',
+      entities: [AirlineEntity, AirportEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
