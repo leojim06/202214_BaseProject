55ebe5d479ee739d8a488974efd56a35de711e7d
o.jimenez-at-285254948890 <leojim06@gmail.com>
2022-09-15 20:44:11 -0500

    add airline and airport modules and entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AirlineModule } from './airline/airline.module';
+import { AirportModule } from './airport/airport.module';
 
 @Module({
-  imports: [],
+  imports: [AirlineModule, AirportModule],
   controllers: [AppController],
   providers: [AppService],
 })
