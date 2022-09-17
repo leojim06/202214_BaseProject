880abe600009dda7071f1b28280f4b04b05ab729
o.jimenez-at-285254948890 <leojim06@gmail.com>
2022-09-16 19:22:37 -0500

    🟢 findAirportsFromAirline invalid airline

--- src/airline-airport/airline-airport.service.ts
+++ src/airline-airport/airline-airport.service.ts
@@ -31,6 +31,8 @@ export class AirlineAirportService {
 
     async findAirportsFromAirline(airlineId: string, airportId: string): Promise<AirportEntity> {
         const airline: AirlineEntity = await this.airlineRepository.findOne({ where: { id: airlineId }, relations: ["airports"] });
+        if (!airline)
+            throw new BusinessLogicException("The airline with the given id was not found", BusinessError.NOT_FOUND);
 
         const airport: AirportEntity = airline.airports.find(a => a.id === airportId);
         if (!airport)
