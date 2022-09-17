8887597581a1d712dda23d7d33ada3a35688f70c
o.jimenez-at-285254948890 <leojim06@gmail.com>
2022-09-17 11:36:55 -0500

    ✈🏨 add airline-airport controller

--- src/airline-airport/airline-airport.module.ts
+++ src/airline-airport/airline-airport.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AirlineEntity } from '../airline/airline.entity';
 import { AirportEntity } from '../airport/airport.entity';
 import { AirlineAirportService } from './airline-airport.service';
+import { AirlineAirportController } from './airline-airport.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AirlineEntity, AirportEntity])],
-  providers: [AirlineAirportService]
+  providers: [AirlineAirportService],
+  controllers: [AirlineAirportController]
 })
 export class AirlineAirportModule { }
